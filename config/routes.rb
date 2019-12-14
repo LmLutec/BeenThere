78aa9df623be2260f912237c9cf862c4bad19555
Lauren McCoy <lm_lutec@outlook.com>
2019-12-14 11:27:23 -0500

    added /signup and /login to config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   resources :transportations
   resources :users
 
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
   resources :sessions, only: [:new, :create]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
