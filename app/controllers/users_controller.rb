783a4aa06a9dcd78f929fa0a49289b14ab4718d8
Lauren McCoy <lm_lutec@outlook.com>
2019-12-15 12:04:06 -0500

    sessions#create #authenticate works correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     def create
         @user = User.create(user_params)
          @user.save
-         byebug
+        
         if @user.id 
             session[:user_id] = @user.id
             redirect_to user_path(@user)
