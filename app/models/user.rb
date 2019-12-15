af9a24b96c3af331a1cbad3c090a3e1c8d392311
Lauren McCoy <lm_lutec@outlook.com>
2019-12-15 11:28:15 -0500

    added presence validations for attributes of user. checked each one by one. Also added a uniquesness validator on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
     has_many :reviews, through: :vacations
     has_many :transportations, through: :vacations
 
+    validates :first_name, :last_name, :age, :email, :password, presence: true 
+    validates :email, uniqueness: true 
     
 end
