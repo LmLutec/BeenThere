d1a295b22d584e63f11923b2c36598c174efca29
Lauren McCoy <lm_lutec@outlook.com>
2019-12-20 19:32:49 -0500

    working on review views

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,13 @@ class Review < ApplicationRecord
     belongs_to :user 
     belongs_to :location 
     
+    helper_method :by_location
 
 
+    def by_location
+        "location.country + location.state + location.city"
+    end 
+
+   
  
 end
