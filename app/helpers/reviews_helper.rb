cbf4e1ae49976141d3ab94d20414c423cf4d9ae1
Lauren McCoy <lm_lutec@outlook.com>
2019-12-25 06:25:49 -0500

    added code to my avg_satisfied helper to get a percentage

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -3,7 +3,8 @@ module ReviewsHelper
 
     def avg_satisfied(reviews)
         @reviews = Location.find_by(id: params[:id]).reviews 
-        @reviews.average(:satisfaction).round 
+        a = @reviews.average(:satisfaction) / 4 * 100
+        number_to_percentage(a, strip_insignificant_zeros:true)     
     end 
 
     def avg_revisit(location)
