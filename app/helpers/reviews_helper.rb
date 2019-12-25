6fa4ba58198a3c653fbbf9605af76b2848d8d80d
Lauren McCoy <lm_lutec@outlook.com>
2019-12-25 07:14:28 -0500

    using details to display reviews. Ordered them by occasion

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -9,7 +9,8 @@ module ReviewsHelper
 
     def avg_revisit(location)
         @reviews = location.reviews 
-        @reviews.average(:revisit).round 
+        a = @reviews.average(:revisit) / 2 * 100 
+        number_to_percentage(a, strip_insignificant_zeros:true)
     end 
 
 end
