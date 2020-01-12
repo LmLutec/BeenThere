51f70a6006af782bec5cb9282ee0953be80b9001
Lauren McCoy <lm_lutec@outlook.com>
2020-01-12 06:48:52 -0500

    locations edit view errors work correctly

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -58,8 +58,12 @@ class LocationsController < ApplicationController
         if @user.locations.include?(@location)
             @r = Review.find_by(location_id: @location, user_id: @user)
             @new = Location.create(location_params)
-            @new.reviews << @r 
-            redirect_to location_path(@new)
+            if @new.save
+                @new.reviews << @r 
+                redirect_to location_path(@new)
+            else 
+                render 'locations/edit'
+            end 
         end
         
     end 
