07a308c6f8d153d7fa9683b5d1ee3106378cf65f
Lauren McCoy <lm_lutec@outlook.com>
2021-02-16 17:10:27 -0500

    dropped table and tried again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_235104) do
+ActiveRecord::Schema.define(version: 2021_02_16_212322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,23 @@ ActiveRecord::Schema.define(version: 2020_07_30_235104) do
     t.text "city"
     t.text "state"
     t.text "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "occasion"
+    t.integer "satisfaction"
+    t.float "revisit"
+    t.text "suggest"
+    t.text "living"
+    t.text "stay_length"
+    t.integer "food_rating"
+    t.text "events"
+    t.text "additional_info"
+    t.text "cost_level"
+    t.integer "user_id"
+    t.integer "location_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
